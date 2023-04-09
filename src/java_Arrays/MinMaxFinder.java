51e8b36823e5ad13b13a5a8f6056534e2067e7f9
kutayturhan <contact@kutayturhan.com>
2023-04-09 17:57:30 +0300

    MinMaxFinder program added.

---
+++ src/java_Arrays/MinMaxFinder.java
@@ -0,0 +1,42 @@
+/*
+Write a Java program that asks the user to input 10 numbers and stores them in an array.
+Then, find and print the maximum and minimum values in the array.
+ */
+
+package java_Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MinMaxFinder {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int[] arr = new int[10];
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        System.out.println("Please enter 10 numbers.");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Number " + (i + 1) + ": ");
+            int nums = scan.nextInt();
+            arr[i] = nums;
+
+            if (arr[i] > max){
+                max = arr[i];
+            }
+            if (arr[i] < min){
+                min = arr[i];
+            }
+        }
+
+        System.out.println("You array is: " + Arrays.toString(arr));
+        System.out.println("Min number in this array is: " + min);
+        System.out.println("Max number in this array is: " + max);
+
+        scan.close();
+    }
+}
